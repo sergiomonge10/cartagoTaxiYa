c900ed953ad4bd6a89bb88bb597408dce793512c
sergiomonge10 <sergiomonge10@gmail.com>
2013-11-11 20:03:32 -0600

    JSON URL

--- settings.py
+++ settings.py
@@ -167,9 +167,9 @@ REST_FRAMEWORK = {
 
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissions'
-    ]
+#    'DEFAULT_PERMISSION_CLASSES': [
+#        'rest_framework.permissions.DjangoModelPermissions'
+#    ]
 }
 
 # A sample logging configuration. The only tangible logging
